83ec1c26523f7526a330084dfcb4e3d8055912ce
root <isaiahnhan@gmail.com>
2024-04-05 16:47:38 +0000

    Added error handling for internal server errors when receiving preflight CORS requests (OPTIONS)

--- app.py
+++ app.py
@@ -4,13 +4,19 @@ import login
 import json
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": ["http://159.203.80.189", "http://localhost:3000", "https://somethingorother.xyz"]}}, supports_credentials=True)
+CORS(app, resources={r"/*": {"origins": ["https://lobster-app-g8oyg.ondigitalocean.app", "http://159.203.80.189", "http://localhost:3000", "https://somethingorother.xyz"]}}, supports_credentials=True)
 
 @app.route('/login', methods=['POST'])
 def login_route():
 	data = request.get_json()
 	result = login.login_handler(data)
 	return result
+
+@app.errorhandler(500)
+def handle_500(e):
+	response = make_response(str(e), 500)
+	response.headers.add('Access-Control-Allow-Origin', '*')
+	return response
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8000)
