2f35cf28ae802577365ab4d976e2cfff4914ef2e
root <isaiahnhan@gmail.com>
2024-04-05 00:47:25 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+from flask_cors import CORS
+import login
+import json
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/login', methods=['POST'])
+def login_route():
+	data = request.get_json()
+	event = {
+		'body': json.dumps(data)
+	}
+	return login.lambda_handler(event, None)
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=8000)
+
+
