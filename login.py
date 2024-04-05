d9f956852ea0a24102f81159e18752f6cc399bb6
isake6 <isaiahnhan@gmail.com>
2024-04-05 14:47:43 -0400

    Update login.py
    Changed login credentials to use email instead of username. Added more returned fields from successful login.

--- login.py
+++ login.py
@@ -15,22 +15,22 @@ db_connection = psycopg2.connect(
 
 def login_handler(data):
     # Get the username and password from the request
-    username = data['username']
+    email = data['email']
     password = data['password']
 
     print('Received login request:', data)
 
     # Input validation
     # If input is missing, return an error
-    if username is None:
-        return jsonify({'message': 'Username is missing'}), 400
+    if email is None:
+        return jsonify({'message': 'email is missing'}), 400
     
     if password is None:
         return jsonify({'message': 'Password is missing'}), 400
     
     # Check if the user exists in the database
     cursor = db_connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    cursor.execute("SELECT * FROM users WHERE username = %s", (username,))
+    cursor.execute("SELECT * FROM users WHERE email = %s", (email,))
     user = cursor.fetchone()
     cursor.close()
 
@@ -43,4 +43,4 @@ def login_handler(data):
         return jsonify({'message': 'Incorrect password'}), 401
     
     # If the login is successful, return the user's information
-    return jsonify({'username': user['username'], 'email': user['email'], 'role': user['role']}), 200
+    return jsonify({'first_name': user['first_name'], 'last_name': user['last_name'], 'email': user['email'], 'role': user['role']}), 200
