fa9a400edf438f6dc2c8f6a0302f1018278535af
isake6 <isaiahnhan@gmail.com>
2024-04-13 20:04:02 -0400

    Update get_single_rso_details.py
    fixed a typo

--- get_single_rso_details.py
+++ get_single_rso_details.py
@@ -28,11 +28,11 @@ def get_single_rso_details_handler(data):
     # Validate rso id
     try:
         cursor = db_connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-        cursor.execute('SELECT * FROM rsos WHERE id = %s', (rso_id,))
+        cursor.execute('SELECT * FROM rso WHERE id = %s', (rso_id,))
         rso = cursor.fetchone()
     except psycopg2.Error as e:
         print(f"Error: {e}")
-        return jsonify({'message': 'Error while trying to select from rsos table.'}), 500
+        return jsonify({'message': 'Error while trying to select from rso table.'}), 500
     finally:
         if cursor is not None:
             cursor.close()
